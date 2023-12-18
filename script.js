ac915449eadadb972e5a08da1573afc7e1044e58
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-18 23:27:40 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container')
+const projects = [
+    '01-GoalList',
+]
+
+
+for (let i = 0; i < projects.length; i++) {
+    container.innerHTML += `
+        <div>
+            <img src=${'./' + projects[i] + '/screen.jpg'} />
+            <h3>${projects[i].split('-')[1]} Project</h3>
+            <a href=${'./' + projects[i] + '/public/index.html'}>
+                <button>View Project</button>
+            </a>
+        </div>
+    `
+}
