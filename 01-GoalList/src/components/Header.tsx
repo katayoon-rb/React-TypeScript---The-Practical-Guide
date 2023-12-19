880ca6cddee544b4db75ebc2d183ea75090572c8
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-19 12:09:57 +0330

    Update Header.tsx

--- 01-GoalList/src/components/Header.tsx
+++ 01-GoalList/src/components/Header.tsx
@@ -1,17 +1,12 @@
 import { ReactNode } from "react"
 
 type HeaderProps = {
-    image: {
-        src: string
-        alt: string
-    }
     children: ReactNode
 }
 
-export default function Header({ image, children }: HeaderProps) {
+export default function Header({ children }: HeaderProps) {
     return (
         <header>
-            <img src={image.src} alt={image.alt} />
             {children}
         </header>
     )
