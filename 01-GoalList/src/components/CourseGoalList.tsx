cc3b18de52834d7ac66cf0d2fbe472e991e7e404
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-19 11:41:21 +0330

    Update CourseGoalList.tsx

--- 01-GoalList/src/components/CourseGoalList.tsx
+++ 01-GoalList/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal.tsx"
+import InfoBox from "./InfoBox.tsx"
 import { type courseGoal } from "../App.tsx"
+import { type ReactNode } from "react"
 
 
 type CourseGoalListProps = {
@@ -8,15 +10,27 @@ type CourseGoalListProps = {
 }
 
 export default function CourseGoalList({goals, onDeleteGoal}: CourseGoalListProps) {
+    if (goals.length === 0) {
+        return <InfoBox mode="hint">You have no goals yet</InfoBox>
+    }
+
+    let warningBox: ReactNode 
+    if (goals.length >= 4) {
+        warningBox = <InfoBox mode="warning">Don't put so much pressure on yourself.</InfoBox>
+    }
+
     return (
-        <ul>
-            {goals.map(goal => (
-                <li key={goal.id}>
-                    <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-                        <p>{goal.desc}</p>
-                    </CourseGoal>
-                </li>
-            ))}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map(goal => (
+                    <li key={goal.id}>
+                        <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+                            <p>{goal.desc}</p>
+                        </CourseGoal>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
