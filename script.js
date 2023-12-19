0ba6737b6c5e7bddb948c2767077378828fdd9f6
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-19 16:17:07 +0330

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ for (let i = 0; i < projects.length; i++) {
         <div>
             <img src=${'./' + projects[i] + '/screen.jpg'} />
             <h3>${projects[i].split('-')[1]} Project</h3>
-            <a href=${'./' + projects[i] + '/index.html'}>
-                <button>View Project</button>
-            </a>
-            <a href=${'https://github.com/katayoon-rb/React-TypeScript-The-Practical-Guide/tree/main/' + projects[i]}>
-                <button>View Code</button>
-            </a>
+            <div class="buttons">
+                <a href=${'./' + projects[i] + '/index.html'}>
+                    <button>View Project</button>
+                </a>
+                <a href=${'https://github.com/katayoon-rb/React-TypeScript-The-Practical-Guide/tree/main/' + projects[i]}>
+                    <button>View Code</button>
+                </a>
+            </div>
         </div>
     `
 }
